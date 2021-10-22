775141f7cf8680a78b560c46cbef9d909fc4fed9
Maxime Princelle <maxime@princelle.org>
2021-10-22 09:15:35 +0200

    📝 revert README to custom

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,6 @@ module.exports = {
       favicon32: "favicon-32x32.png",
       favicon16: "favicon-16x16.png",
       appleTouchIcon: "apple-touch-icon.png",
-      maskIcon: "safari-pinned-tab.svg",
       msTileImage: "mstile-150x150.png",
     },
   },
